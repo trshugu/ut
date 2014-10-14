dbdb16eb1067d1c7c751d95f8d87c7a66081ce00
anokutara <anokutara@sanmyaku.com>
2014-10-14 11:23:02 +0900

    gitpush2014/10/14

---
+++ r/rrun.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\R\R-3.1.1\bin\x64\Rscript.exe" tmp.r
---
+++ r/tmp.r
@@ -0,0 +1,23 @@
+if(FALSE){
+}
+
+
+
+
+
+
+if(FALSE){
+1 + 2
+a <- 3
+comment(a) <- "komeeento"
+comment(a)
+b <- 4
+comment(b)
+a + b
+s <- "asdffs"
+s
+# Sys.getlocale()
+n <- "ddd"
+n
+}
+
