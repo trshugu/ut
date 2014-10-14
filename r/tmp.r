fbbc349d9740b9fc1279727af93e9d57b785ac2b
anokutara <anokutara@sanmyaku.com>
2014-10-14 11:35:42 +0900

    gitpush2014/10/14

--- r/tmp.r
+++ r/tmp.r
@@ -5,6 +5,19 @@ if(FALSE){
 
 
 
+if(FALSE){
+# データの読み込み
+sake <- read.table("./sake_dataJ.txt", header=T, row.names=1)
+names(sake) <- c("日本酒","焼酎","ビール","ワイン","ウイスキー")
+
+# データ間のユークリッド距離を算出
+distance <- dist(sake)
+# ウォード法を用いてクラスタ間の距離を算出
+cluster <- hclust(distance, method="ward.D2")
+# ヒートマップを表示
+heatmap(as.matrix(sake), Rowv=as.dendrogram(cluster), col=colorRampPalette(c("white", "purple"))(256), margin=c(4,0), cexCol=0.8, cexRow=0.7)
+}
+
 
 if(FALSE){
 1 + 2
