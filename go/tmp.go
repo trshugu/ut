63f14260589be91c57a36235fdfb1af17e96fc8c
anokutara <anokutara@sanmyaku.com>
2014-11-04 17:26:33 +0900

    gitpush2014/11/04

--- go/tmp.go
+++ go/tmp.go
@@ -1,11 +1,105 @@
 package main
 import "fmt"
-func main(){
+import "strings"
+func main() {
 /*
 */
 
 
+
+/*
+文字出力
 fmt.Printf("Hell, World\n")
+*/
+
+/*
+*/
+// 文字列操作
+fmt.Printf(strings.ToLower("aabbccdd\n"));
+fmt.Printf(strings.ToUpper("aabbccdd\n"));
+
+
+/*
+*/
+// 文字列検索
+
+
+/*
+*/
+// 数値、数字 計算
+
+
+/*
+*/
+// 配列
+
+
+/*
+*/
+// 連想配列
+
+
+/*
+*/
+// 配列の検索
+
+
+/*
+*/
+// 条件分岐
+
+
+/*
+*/
+// case分岐
+
+
+/*
+*/
+// 繰り返し
+
+
+/*
+*/
+// 無限繰り返し
+
+
+/*
+*/
+// foreach
+
+
+/*
+*/
+// クラス
+
+
+/*
+*/
+// 無名関数
+
+
+/*
+*/
+// ラムダ
+
+
+/*
+*/
+// クロージャ
+
+
+/*
+*/
+// jsonの取扱
+
+
+/*
+*/
+// restの取扱
+
+
+
 
 /*
 // hell world
@@ -15,14 +109,20 @@ fmt.Printf("Hello, World\n")
 } /* ------------------------------------------- */
 
 /*
-*/
-
 // 型(構造体)の定義
 type Something struct {
   member int
 }
- 
+*/
+
+/*
 // メソッドの定義
 func ( this *Something ) Function() {
   fmt.Printf( "%d", this.member );
 }
+*/
+
+func init() {
+  fmt.Printf("init\n");
+}
+
