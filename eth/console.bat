daa7712b094965ca855621dbde9fbf98021df0ed
anokutara <a@a.a>
2019-10-31 23:53:53 +0900

    gitpush2019/10/31

--- eth/console.bat
+++ eth/console.bat
@@ -2,4 +2,4 @@
 rem geth --networkid "15" --nodiscover --datadir "./"
 rem geth  --rpc --rpcport 8545 --rpcapi "web3,eth,net,personal,miner" --rpccorsdomain "*" --rpcaddr "0.0.0.0" --datadir "./" --nodiscover --networkid 15
 rem geth  --rpc --rpcport 8545 --rpcapi "web3,eth,net,personal,miner" --rpccorsdomain "*" --rpcaddr "0.0.0.0" --datadir "./" --nodiscover --networkid 15
-geth --datadir "./" attach ipc:\\.\pipe\geth.ipc
+geth --networkid "15" --datadir "./" attach ipc:\\.\pipe\geth.ipc
