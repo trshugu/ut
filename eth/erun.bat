4f4049ef4f6f85aa079c144b64b6b0f010bd2624
aaa <aa@a>
2019-10-31 12:38:41 +0900

    gitpush2019/10/31

--- eth/erun.bat
+++ eth/erun.bat
@@ -2,6 +2,7 @@
 rem geth --networkid "15" --nodiscover --datadir "./"
 rem geth  --rpc --rpcport 8545 --rpcapi "web3,eth,net,personal,miner" --rpccorsdomain "*" --rpcaddr "0.0.0.0" --datadir "./" --nodiscover --networkid 15
 rem geth  --rpc --rpcport 8545 --rpcapi "web3,eth,net,personal,miner" --rpccorsdomain "*" --rpcaddr "0.0.0.0" --datadir "./" --nodiscover --networkid 15 --allow-insecure-unlock
+rem geth --networkid "15" --nodiscover --datadir "./" console 2>> ./geth_err.log
 geth --datadir ./ init ./g.json
 geth  --rpc --rpcport 8545 --rpcapi "web3,eth,net,personal,miner" --rpccorsdomain "*" --rpcaddr "0.0.0.0" --datadir "./" --nodiscover --networkid 15
 
