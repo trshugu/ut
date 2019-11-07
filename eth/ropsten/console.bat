bfd37dd4613101264769178d8a84b56706178b27
aaa <aa@a>
2019-11-07 15:06:23 +0900

    gitpush2019/11/07

---
+++ eth/ropsten/console.bat
@@ -0,0 +1,8 @@
+@echo off
+rem geth --networkid "15" --nodiscover --datadir "./"
+rem geth  --rpc --rpcport 8545 --rpcapi "web3,eth,net,personal,miner" --rpccorsdomain "*" --rpcaddr "0.0.0.0" --datadir "./" --nodiscover --networkid 15
+rem geth  --rpc --rpcport 8545 --rpcapi "web3,eth,net,personal,miner" --rpccorsdomain "*" --rpcaddr "0.0.0.0" --datadir "./" --nodiscover --networkid 15
+rem geth --networkid "15" --datadir "./" attach ipc:\\.\pipe\geth.ipc
+rem geth.exe --networkid "15" --datadir "./" attach ipc:\\.\pipe\geth.ipc
+geth.exe attach ipc:\\.\pipe\geth.ipc
+
---
+++ eth/ropsten/erun.bat
@@ -0,0 +1,10 @@
+@echo off
+rem geth --networkid "15" --nodiscover --datadir "./"
+rem geth  --rpc --rpcport 8545 --rpcapi "web3,eth,net,personal,miner" --rpccorsdomain "*" --rpcaddr "0.0.0.0" --datadir "./" --nodiscover --networkid 15
+rem geth --networkid "15" --nodiscover --datadir "./" console 2>> ./geth_err.log
+rem geth --datadir ./ init ./g.json
+rem geth  --rpc --rpcport 8545 --rpcapi "web3,eth,net,personal,miner" --rpccorsdomain "*" --rpcaddr "0.0.0.0" --datadir "./" --nodiscover --networkid 15
+rem geth  --rpc --rpcport 8545 --rpcapi "web3,eth,net,personal,miner" --rpccorsdomain "*" --rpcaddr "0.0.0.0" --datadir "./" --nodiscover --networkid "15" --allow-insecure-unlock
+rem geth.exe  --rpc --rpcport 8545 --rpcapi "web3,eth,net,personal,miner" --rpccorsdomain "*" --rpcaddr "0.0.0.0" --datadir "./" --nodiscover --networkid "15"
+geth.exe --datadir "./" --testnet --syncmode "fast"
+
